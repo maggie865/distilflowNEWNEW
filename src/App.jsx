e79b00c0b158ee3cc7faa8bea26fd0722194883b
Bluff Distillery <info@bluffdistillery.com>
2026-06-12 00:13:21 +0000

    Manual code change

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+console.log('APP_ID', import.meta.env.VITE_BASE44_APP_ID);
+console.log('BASE_URL', import.meta.env.VITE_BASE44_APP_BASE_URL);
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
